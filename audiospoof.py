39e380294f00f8d895841e9673f4d94fc10e7b95
Clashhub <Clashhub@users.noreply.github.com>
2017-07-14 09:54:59 +0800

    Add files via upload

---
+++ audiospoof.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 19 23:23:53 2017
+
+@author: Nigel Wee
+
+This whole thing takes about 10mins to compile 
+"""
+import os
+import numpy as np
+from sklearn import mixture
+import librosa
+import matplotlib.pyplot as plt
+import functions as fn
+
+# The paths where the full training files are stored
+path1 = r"C:\train_dev_test\training\genuine"
+path2 = r"C:\train_dev_test\training\spoofed"
+
+#Partial training files
+#path1 = r"C:\train_subdivided_samples\genuine"
+#path2 = r"C:\train_subdivided_samples\spoofed"
+
+# A list of all audiofiles 
+audiofile1 = os.listdir(path1)  # genuine
+audiofile2 = os.listdir(path2)  # spoofed
+
+sum = 0
+gen_list = []
+spo_list = []
+frames = 100
+
+# Extract Mfcc features from all audio files
+for filename in audiofile1:
+    
+   # Concatenate path with filename 
+   filename = os.path.join(path1, filename)
+   
+   # Load the audiofile
+   y, sr = librosa.load(filename) # y = audio time series, sr = sampling rate
+   
+   # Extract MFCC features and append
+   mfcc = librosa.feature.mfcc(y=y, sr=sr)
+   sum += len(mfcc[0])
+   
+   # Extract MFCC Delta and append
+   mfcc_delta = librosa.feature.delta(mfcc)
+  
+   # Extract MFCC Delta-Delta and append
+   mfcc_delta_delta = librosa.feature.delta(mfcc_delta)
+     
+   a = np.vstack([mfcc, mfcc_delta, mfcc_delta_delta])
+   
+   if len(a[0]) < frames:
+       a = np.pad(a, [(0,0),(0, frames-len(a[0]))], 'constant')
+   elif len(a[0]) > frames:
+       a = a[0:60, :frames]
+       
+   gen_list.append(np.transpose(a))
+        
+genuine = np.vstack(gen_list)      
+
+sum = 0
+
+for filename in audiofile2:
+   
+   # Concatenate path with filename 
+   filename = os.path.join(path2, filename)
+   
+   # Load the audio file
+   y, sr = librosa.load(filename) # y = audio time series, sr = sampling rate
+   
+   # Extract MFCC features
+   mfcc = librosa.feature.mfcc(y=y, sr=sr)
+   sum += len(mfcc[0])
+   
+   # Extract MFCC Delta
+   mfcc_delta = librosa.feature.delta(mfcc)
+
+   # Extract MFCC Delta-Delta
+   mfcc_delta_delta = librosa.feature.delta(mfcc_delta)
+   
+   b = np.vstack([mfcc, mfcc_delta, mfcc_delta_delta])
+   
+   if len(b[0]) < frames:
+       b = np.pad(b, [(0,0),(0, frames-len(b[0]))], 'constant')
+   elif len(b[0]) > frames:
+       b = b[0:60, :frames]
+       
+   spo_list.append(np.transpose(b))
+   
+spoofed = np.vstack(spo_list) 
+
+gmm1 = mixture.GaussianMixture(n_components = 20)
+gmm1.fit(genuine)
+#
+gmm2 = mixture.GaussianMixture(n_components = 20)
+gmm2.fit(spoofed)  
+
+ 
---
+++ functions.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 23 12:44:19 2017
+
+@author: Nigel Wee
+
+"""
+import numpy as np
+import librosa
+
+class DataHolder(object):
+    def __init__(self, mfcc, delta, deldel, previous = None):        
+        self.data = np.vstack([mfcc, delta, deldel])
+        self.previous = previous     
+        
+def calMeanOfFrames(object):
+   sum = 0
+   x = 0
+   feature_object = object
+   while feature_object.previous != None:
+       x += 1
+       sum += len(feature_object.data[0])
+       feature_object = feature_object.previous
+    
+   #For the first object 
+   sum += len(feature_object.data[0])   
+   x += 1
+   
+   mean = sum // x 
+   return mean
+
+def vStackFeatures(mean, object):
+    feature_object = object
+    
+    if len(feature_object.data[0]) < mean:
+      feature_object.data = np.pad(feature_object.data, [(0,0),(0, mean-len(feature_object.data[0]))], 'constant')
+    
+    a = np.array(feature_object.data[0:20, :mean])
+    feature_object = feature_object.previous
+    
+    while feature_object.previous != None:
+        if len(feature_object.data[0]) < mean:
+            feature_object.data = np.pad(feature_object.data, [(0,0),(0, mean-len(feature_object.data[0]))], 'constant')
+            
+        a = np.vstack((feature_object.data[0:20, :mean],a))
+        feature_object = feature_object.previous
+     
+    if len(feature_object.data[0]) < mean:
+      feature_object.data = np.pad(feature_object.data, [(0,0),(0, mean-len(feature_object.data[0]))], 'constant')  
+      
+    a = np.vstack((feature_object.data[0:20, :mean],a)) 
+    
+    return a
+    
+def getFeatures(frames):
+  # For the first file only   
+  # Load the audiofile
+  y, sr = librosa.load(r"C:\train_dev_test\devolpment\genuine\D_1000001.wav") # y = audio time series, sr = sampling rate
+    
+  # Extract MFCC features and append
+  mfcc = librosa.feature.mfcc(y=y, sr=sr)
+   
+  # Extract MFCC Delta and append
+  mfcc_delta = librosa.feature.delta(mfcc)
+    
+  # Extract MFCC Delta-Delta and append
+  mfcc_delta_delta = librosa.feature.delta(mfcc_delta)
+
+  c = np.vstack([mfcc, mfcc_delta, mfcc_delta_delta])
+  if len(c[0]) < frames:
+       c = np.pad(c, [(0,0),(0, frames-len(c[0]))], 'constant')
+  elif len(c[0]) > frames:
+       c = c[0:60, :frames]
+        
+  return np.transpose(c)
